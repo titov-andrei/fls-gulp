06119c546549b7fbee8dd728dd8e377cb4e5880f
titov-andrei <titov_andrei@mail.ru>
2020-12-05 20:42:21 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,17 @@ let path = {
     img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     fonts: source_folder + "/fonts/*.ttf",
   },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  clean: "./" + project_folder + "/"
 };
+
+let { src, dest } = require('gulp'),
+gulp = require('gulp'),
+browsersync = require("browser-sync").create();
+
+
