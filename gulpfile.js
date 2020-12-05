cb9c25cf41ce40381d1147cf6aa4ef4a922e079a
titov-andrei <titov_andrei@mail.ru>
2020-12-05 20:16:02 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,15 @@ let path = {
   build: {
     html: project_folder + "/",
     css: project_folder + "/css/",
-  }
-}
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+  },
+  src: {
+    html: source_folder + "/",
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/script.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
+};
