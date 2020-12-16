3beef82fed1c5dab0cc56a0ec993a111822e54a4
titov-andrei <titov_andrei@mail.ru>
2020-12-16 18:44:44 +0300

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let path = {
     fonts: project_folder + "/fonts/",
   },
   src: {
-    html: source_folder + "/*.html",
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
     css: source_folder + "/scss/style.scss",
     js: source_folder + "/js/script.js",
     img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
