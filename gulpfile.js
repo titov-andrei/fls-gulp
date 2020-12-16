8f8c38a519e30c50bcf5b201684fe43c3ad63190
titov-andrei <titov_andrei@mail.ru>
2020-12-16 20:41:23 +0300

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,11 @@ let { src, dest } = require("gulp"),
   browsersync = require("browser-sync").create(),
   fileinclude = require("gulp-file-include"),
   del = require("del"),
-  scss = require("gulp-sass");
+  scss = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename");
 
 function browserSync(params) {
   browsersync.init({
@@ -51,9 +55,25 @@ function html() {
 
 function css() {
   return src(path.src.css)
-    .pipe(scss({
-      outputStyle: "expanded"
-    }))
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(group_media())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true,
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
     .pipe(dest(path.build.css))
     .pipe(browsersync.stream());
 }
